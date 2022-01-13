78aa4f43d78d27cf8f958a6ea9a273802079cf44
Ansh Narula <2019210@iiitdmj.ac.in>
2022-01-14 01:32:38 +0530

    deploy-3

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/api/users", userRoutes)
 app.use("/api/review", reviewRoutes)
 app.use("/api/upload", uploadRoute)
 
-const PORT = 5000 || process.env.PORT
+const PORT =  process.env.PORT || 5000
 
 app.use(notFound)
 
